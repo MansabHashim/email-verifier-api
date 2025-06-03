7b8ece1fc0f8415d145f3d8303044c882bd18f27
Mansab Hashim <112323899+MansabHashim@users.noreply.github.com>
2025-06-03 20:14:27 +0500

    rewrote the code

--- app.py
+++ app.py
@@ -38,9 +38,3 @@ def verify():
         return jsonify({"error": "Missing email"}), 400
     status = verify_email(email)
     return jsonify({"email": email, "status": status})
-
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
