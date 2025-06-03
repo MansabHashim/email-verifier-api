c704c98e24f96f50a560ff234a89d8d94b47a5a0
Mansab Hashim <112323899+MansabHashim@users.noreply.github.com>
2025-06-03 20:08:08 +0500

    d

--- app.py
+++ app.py
@@ -39,3 +39,8 @@ def verify():
     status = verify_email(email)
     return jsonify({"email": email, "status": status})
 
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
